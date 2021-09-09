1f52bb69a80c48502a44f0103067049c434a1fd1
Thongthana Laemkaothong <89116474+TL9Thongthana@users.noreply.github.com>
2021-09-09 21:42:00 +0700

    Update Week4_1.cpp

--- Week4_1.cpp
+++ Week4_1.cpp
@@ -1,23 +1,47 @@
 //64010300
+//เปลี่ยนโจทย์เป็น
+//จงเขียนโปรแกรมเพื่อรับข้อมูลชนิดตัวเลข แล้วตรวจสอบว่าตัวเลขที่ผู้ใช้ใส่เข้ามานั้นเป็นจำนวนเฉพาะหรือไม่ เมื่อแสดงผลลัพธ์แล้วให้กลับไปรอรับข้อมูลใหม่วนอย่างนี้ไปเรื่อยๆ จนกระทั่งผู้ใช้ใส่ค่า -99 จึงจบการทำงาน (Level 2)
 #include<stdio.h>
-#include <math.h>
+int isPrime(int);
 int main()
 {
-	float a,b,c,s;
-	printf("First side : ");
-	scanf("%f",&a);
-	printf("Second side : ");
-	scanf("%f",&b);
-	printf("Third side : ");
-	scanf("%f",&c);
-	if(a>0&&b>0&&c>0)
+	int n;
+	printf("Input Number : ");
+	scanf("%d",&n);
+	while(isPrime(n)==1||isPrime(n)==0)
 	{
-		s=(a+b+c)/2;
-		printf("%.2f",sqrt(s*(s-a)*(s-b)*(s-c)));	
+		if(n!=-99)
+		{
+			if(isPrime(n)==1)
+			{
+				printf("Prime Number\n\n");
+			}
+			else
+			{
+				printf("Not Prime Number\n\n");	
+			}	
+			printf("Input New Number : ");
+			scanf("%d",&n);
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+int isPrime(int x)
+{
+	int i=2;
+	while(i<=x&&x%i!=0)
+	{
+		i=i+1;
+	}
+	if(x==i)
+	{
+		return 1;
 	}
 	else
 	{
-		printf("Error");
+		return 0;
 	}
-	return 0;
 }
