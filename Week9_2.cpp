42945ea4adc94e5d1b18a8592a6673003058cadd
Thongthana Laemkaothong <89116474+TL9Thongthana@users.noreply.github.com>
2021-10-09 00:03:23 +0700

    Add files via upload

---
+++ Week9_2.cpp
@@ -0,0 +1,30 @@
+//64010300
+#include<stdio.h>
+#include<string.h>
+void find(char* n)
+{
+	int i,d,c,No_U=0,No_l=0;
+	for(i=0;i<strlen(n);++i)
+	{
+		c=n[i];
+		if(c>=65&&c<=90)
+		{
+			No_U=No_U+1;
+		}
+		else if(c>=97&&c<=122)
+		{
+			No_l=No_l+1;
+		}
+	}
+	printf("Upper = %d \nlower = %d",No_U,No_l);
+}
+int main()
+{
+	int k;
+	char n[10000];
+	printf("Input String : ");
+	scanf("%s",n);
+	find(n);	
+	
+	return 0;
+}
