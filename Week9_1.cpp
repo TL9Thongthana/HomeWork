4d0091bb294e99329fdd612569e358cb75980853
Thongthana Laemkaothong <89116474+TL9Thongthana@users.noreply.github.com>
2021-10-09 00:07:50 +0700

    Add files via upload

---
+++ Week9_1.cpp
@@ -0,0 +1,25 @@
+//64010300
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,No_U=0,No_l=0;
+	char n[10000],c;
+	printf("Input String : ");
+	scanf("%s",n);
+	for(i=0;i<strlen(n);++i)
+	{
+		c=n[i];
+		if(c>=65&&c<=90)
+		{
+			No_U=No_U+1;
+		}
+		else if(c>=97&&c<=122)
+		{
+			No_l=No_l+1;
+		}
+	}
+	printf("Upper = %d \nlower = %d",No_U,No_l);
+	
+	return 0;
+}
